73129d67ea8b9299cb1c5b822480b43fb6d8a42b
eminetics <dylansbartussek@gmail.com>
2026-02-17 02:35:00 +0000

    upload for EOD

--- bash/bootstrap.sh
+++ bash/bootstrap.sh
@@ -1,6 +1,11 @@
 #Set bash
 #!/bin/bash
 
+# Color Variables
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color (Resets the terminal to default)
+
 #Set to exit or error
 set -e
 
@@ -10,19 +15,23 @@ export DEBIAN_FRONTEND=noninteractive
 #Variables
 PACKAGES=(python3 python3-apt wget curl net-tools vim)
 
-echo "Environment Is set On $(hostname) Proceeding To Package Installation"
+echo "${GREEN}---Environment Is set On $(hostname) Proceeding To Package Installation---${NC}"
 
 #Refresh Package Lists
 sudo apt-get update -y
 
 #Start Installing Packages
-for package in "${PACKAGES[@]}"
-do
-	sudo apt-get install -y "$package"
-	echo "---$package Installed on $(hostname)"
-done
+#for package in "${PACKAGES[@]}"
+#do
+#	sudo apt-get install -y "$package"
+#	echo "${GREEN}---$package Installed on $(hostname)---${NC}"
+#	if 
+	
+#	fi
+#done
 
-
+#Smarter Loop
+#for, if, else find if its installed and if not install it
 
 
 
